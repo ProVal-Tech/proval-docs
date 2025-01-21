97b811a4cd3b543e221c923d38f987605fe6b967
Stephen <11358399+nixuno@users.noreply.github.com>
2025-01-21 15:58:00 -0500

    Change brokenLinks to warn for testing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'proval-tech', // Usually your GitHub org/user name.
   projectName: 'proval-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
