7e03e027d4c84bf5af5c6db7e4f882673ebd36d2
Stephen <11358399+nixuno@users.noreply.github.com>
2025-03-25 15:51:02 -0400

    Finish script
    Finish script
    
    Finish script

--- helper_scripts/Resolve-LinksToSlugs.ps1
+++ helper_scripts/Resolve-LinksToSlugs.ps1
@@ -6,5 +6,9 @@ foreach($doc in $docs) {
     $targetLinks = $content | Select-String -Pattern "\[.*?\]\(<?(.*?\.md)>?\)" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value } | Sort-Object | Get-Unique
     foreach($link in $targetLinks) {
         $targetLinkItem = $link | Resolve-Path | Get-Item
+        $slug = Get-Content $targetLinkItem.FullName | Select-String -Pattern "slug: (.*)" | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value }
+        $content = $content -replace "<?$([regex]::Escape($link))>?", "/docs$slug"
     }
+    $content | Out-File $doc.FullName
+    Pop-Location
 }
