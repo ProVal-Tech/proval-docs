ad7220a9d592a0da861b85bcb5d17fab8f589867
Stephen <11358399+nixuno@users.noreply.github.com>
2025-03-26 09:34:06 -0400

    Update script

--- helper_scripts/Resolve-LinksToSlugs.ps1
+++ helper_scripts/Resolve-LinksToSlugs.ps1
@@ -3,9 +3,18 @@ $docs = Get-ChildItem $docsPath -Recurse -Filter "*.md"
 foreach($doc in $docs) {
     Push-Location -Path $doc.DirectoryName
     $content = Get-Content $doc.FullName
-    $targetLinks = $content | Select-String -Pattern "\[.*?\]\(<?(.*?\.md)>?\)" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value } | Sort-Object | Get-Unique
+    $targetLinks = $null
+    $targetLinks = $content | Select-String -Pattern "\[.*?\]\(<?(.*?\.mdx?)>?\)" -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value -replace "\.mdx",".md" } | Sort-Object | Get-Unique
+    if($targetLinks.Count -eq 0 -or $null -eq $targetLinks) {
+        Pop-Location
+        continue
+    }
     foreach($link in $targetLinks) {
+        $targetLinkItem = $null
         $targetLinkItem = $link | Resolve-Path | Get-Item
+        if($null -eq $targetLinkItem) {
+            continue
+        }
         $slug = Get-Content $targetLinkItem.FullName | Select-String -Pattern "slug: (.*)" | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value }
         $content = $content -replace "<?$([regex]::Escape($link))>?", "/docs$slug"
     }
