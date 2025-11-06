7544a03af89e85e3c79c4a42f1f7f75cf0a763b4
Stephen Nix <11358399+s-nix@users.noreply.github.com>
2025-11-06 09:33:23 -0500

    Implement code changes to enhance functionality and improve performance

--- helper_scripts/Update-RecentDocList.ps1
+++ helper_scripts/Update-RecentDocList.ps1
@@ -1,9 +1,20 @@
 $docsPath = (Get-Item $PSScriptRoot).Parent.FullName + "\docs"
 $docs = Get-ChildItem $docsPath -Recurse -Filter "*.md"
 $recentDocs = $docs | ForEach-Object {
+    $parentPath = Split-Path $_.FullName
+    if ((Get-Item $parentPath).Name -in ("docs", "unsorted")) {
+        return
+    }
+    if ((Get-Item (Split-Path $parentPath)).Name -ne "docs") {
+        $parentPath = Split-Path $parentPath
+        while ((Get-Item (Split-Path $parentPath)).Name -ne "docs") {
+            $parentPath = Split-Path $parentPath
+        }
+    }
+    $parentName = (Get-Item $parentPath).Name
     $_ | Add-Member -NotePropertyName "LastCommitTime" -NotePropertyValue (
         [datetime]::Parse((git log -1 --format="%ci" -- $_.FullName 2>$null))
-    ) -PassThru
+    ) -PassThru | Add-Member -NotePropertyName "Category" -NotePropertyValue $parentName -PassThru
 } | Where-Object { $_.LastCommitTime -ne $null } | Sort-Object LastCommitTime -Descending
 $output = foreach ($doc in $recentDocs) {
     $content = Get-Content $doc.FullName
@@ -13,6 +24,7 @@ $output = foreach ($doc in $recentDocs) {
         Title      = if ($title) { $title.Matches[0].Groups[2].Value } else { "No Title" }
         Slug       = if ($slug) { $slug.Matches[0].Groups[2].Value } else { $doc.BaseName }
         LastCommit = $doc.LastCommitTime
+        Category   = $doc.Category
     }
 }
 $output | Where-Object { $_.Slug -match "[0-9A-Fa-f]{8}(?:-[0-9A-Fa-f]{4}){3}-[0-9A-Fa-f]{12}" -and $_.Title.Trim() -ne "" } | ConvertTo-Json | Out-File -FilePath "$((Get-Item $PSScriptRoot).Parent.FullName)\src\pages\recentDocs.json" -Encoding UTF8
